a3a8987160b85cfb601cc464df092309f5b3ec78
Abdelaziz Mahdy <zezohassam@gmail.com>
2022-04-11 03:43:43 +0200

    changed link

--- example/lib/pages/basic_example_page.dart
+++ example/lib/pages/basic_example_page.dart
@@ -38,7 +38,7 @@ class _BasicExamplePageState extends State<BasicExamplePage> {
       DataSource(
         type: DataSourceType.network,
         source:
-            "https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4",
+            "https://movietrailers.apple.com/movies/paramount/the-spongebob-movie-sponge-on-the-run/the-spongebob-movie-sponge-on-the-run-big-game_h720p.mov",
       ),
       autoplay: true,
     );
