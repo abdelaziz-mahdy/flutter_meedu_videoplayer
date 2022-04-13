8869c1a65096636356422a5284bf4c250e076ad9
Abdelaziz Mahdy <zezohassam@gmail.com>
2022-04-14 01:37:20 +0200

    removed safe area

--- lib/src/widgets/styles/primary/primary_player_controls.dart
+++ lib/src/widgets/styles/primary/primary_player_controls.dart
@@ -17,76 +17,74 @@ class PrimaryVideoPlayerControls extends StatelessWidget {
     final _ = MeeduPlayerController.of(context);
 
     return ControlsContainer(
-      child: SafeArea(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            // RENDER A CUSTOM HEADER
-            if (_.header != null)
-              Positioned(
-                left: 0,
-                right: 0,
-                top: 0,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: _.header!,
-                ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          // RENDER A CUSTOM HEADER
+          if (_.header != null)
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              child: Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child: _.header!,
               ),
-            Container(
-              height: context.mediaQuerySize.height,
-              width: context.mediaQuerySize.width,
             ),
+          Container(
+            height: context.mediaQuerySize.height,
+            width: context.mediaQuerySize.width,
+          ),
 
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (_.enabledButtons.rewindAndfastForward) ...[
-                  PlayerButton(
-                    onPressed: _.rewind,
-                    size: responsive.ip(_.fullscreen.value ? 8 : 12),
-                    iconColor: Colors.white,
-                    backgrounColor: Colors.transparent,
-                    iconPath: 'assets/icons/rewind.png',
-                    customIcon: _.customIcons.rewind,
-                  ),
-                  SizedBox(width: 10),
-                ],
-                if (_.enabledButtons.playPauseAndRepeat)
-                  RxBuilder(
-                      //observables: [_.showSwipeDuration],
-                      //observables: [_.swipeDuration],
-                      (__) {
-                    _.dataStatus.status.value;
-                    if (!_.showSwipeDuration.value &&
-                        !_.dataStatus.error &&
-                        !_.dataStatus.loading &&
-                        !_.isBuffering.value) {
-                      return PlayPauseButton(
-                        size: responsive.ip(_.fullscreen.value ? 8 : 13),
-                      );
-                    } else {
-                      return Container();
-                    }
-                  }),
-                if (_.enabledButtons.rewindAndfastForward) ...[
-                  SizedBox(width: 10),
-                  PlayerButton(
-                    onPressed: _.fastForward,
-                    iconColor: Colors.white,
-                    backgrounColor: Colors.transparent,
-                    size: responsive.ip(_.fullscreen.value ? 8 : 12),
-                    iconPath: 'assets/icons/fast-forward.png',
-                    customIcon: _.customIcons.fastForward,
-                  ),
-                ]
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (_.enabledButtons.rewindAndfastForward) ...[
+                PlayerButton(
+                  onPressed: _.rewind,
+                  size: responsive.ip(_.fullscreen.value ? 8 : 12),
+                  iconColor: Colors.white,
+                  backgrounColor: Colors.transparent,
+                  iconPath: 'assets/icons/rewind.png',
+                  customIcon: _.customIcons.rewind,
+                ),
+                SizedBox(width: 10),
               ],
-            ),
+              if (_.enabledButtons.playPauseAndRepeat)
+                RxBuilder(
+                    //observables: [_.showSwipeDuration],
+                    //observables: [_.swipeDuration],
+                    (__) {
+                  _.dataStatus.status.value;
+                  if (!_.showSwipeDuration.value &&
+                      !_.dataStatus.error &&
+                      !_.dataStatus.loading &&
+                      !_.isBuffering.value) {
+                    return PlayPauseButton(
+                      size: responsive.ip(_.fullscreen.value ? 8 : 13),
+                    );
+                  } else {
+                    return Container();
+                  }
+                }),
+              if (_.enabledButtons.rewindAndfastForward) ...[
+                SizedBox(width: 10),
+                PlayerButton(
+                  onPressed: _.fastForward,
+                  iconColor: Colors.white,
+                  backgrounColor: Colors.transparent,
+                  size: responsive.ip(_.fullscreen.value ? 8 : 12),
+                  iconPath: 'assets/icons/fast-forward.png',
+                  customIcon: _.customIcons.fastForward,
+                ),
+              ]
+            ],
+          ),
 
-            PrimaryBottomControls(
-              responsive: responsive,
-            ),
-          ],
-        ),
+          PrimaryBottomControls(
+            responsive: responsive,
+          ),
+        ],
       ),
     );
   }
