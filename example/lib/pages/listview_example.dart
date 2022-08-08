7e1e2753f7f830f0566a8d80fff9a957bf642c93
Abdelaziz Mahdy <zezohassam@gmail.com>
2022-08-08 23:55:57 +0200

    added example helping comments

--- example/lib/pages/listview_example.dart
+++ example/lib/pages/listview_example.dart
@@ -56,6 +56,12 @@ class _VideoItemState extends State<VideoItem>
         source:
             'https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4',
         type: DataSourceType.network,
+        httpHeaders: {
+          // u can change the link to  ur referr
+          "Referer": "https://google.com/",
+          // also u can change the user agent since exo player sometimes is blocked
+          "User-Agent": "animdl/1.5.84",
+        },
       ),
       autoplay: false,
     );
