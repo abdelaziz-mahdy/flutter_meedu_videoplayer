68c664bf1dab8242ec1b9043cad436a59178de06
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-03-31 14:49:51 +0200

    removing not needed files

--- package/example/windows/flutter/generated_plugin_registrant.cc
+++ package/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_meedu_videoplayer/flutter_meedu_videoplayer_plugin.h>
 #include <fullscreen_window/fullscreen_window_plugin_c_api.h>
 #include <media_kit_video/media_kit_video_plugin_c_api.h>
 #include <screen_brightness_windows/screen_brightness_windows_plugin.h>
@@ -14,8 +13,6 @@
 #include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterMeeduVideoplayerPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterMeeduVideoplayerPlugin"));
   FullscreenWindowPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FullscreenWindowPluginCApi"));
   MediaKitVideoPluginCApiRegisterWithRegistrar(
