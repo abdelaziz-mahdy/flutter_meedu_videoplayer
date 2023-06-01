b4d96ce0f10257d23381cbacb25308712616779a
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-06-01 19:40:54 +0300

    on log on error

--- package/lib/init_meedu_player.dart
+++ package/lib/init_meedu_player.dart
@@ -4,11 +4,11 @@ import 'package:universal_platform/universal_platform.dart';
 import 'package:window_manager/window_manager.dart';
 
 Future<void> initMeeduPlayer(
-    {bool iosUseMediaKit = false, bool androidUseMediaKit = false}) async {
+    {bool iosUseMediaKit = false, bool androidUseMediaKit = false, MPVLogLevel logLevel=MPVLogLevel.error}) async {
   WidgetsFlutterBinding.ensureInitialized();
   await initWindowManagerIfNeeded();
   initVideoPlayerMediaKitIfNeeded(
-      iosUseMediaKit: iosUseMediaKit, androidUseMediaKit: androidUseMediaKit);
+      iosUseMediaKit: iosUseMediaKit, androidUseMediaKit: androidUseMediaKit,logLevel: logLevel);
 }
 
 Future<void> initWindowManagerIfNeeded() async {
